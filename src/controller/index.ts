aa02cffd809d6b815f18da37d6a9295ac8cff608
dltjdn <hana40004@naver.com>
2022-11-20 05:36:35 +0900

    [DEL] 필요없는 코드 삭제

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,4 +1,3 @@
-// export { default as MainController } from "./MainController";
 export { default as UserController } from "./UserController";
 export { default as EvaluationController } from "./EvaluationController";
 export { default as ResultController } from "./ResultController";
--- src/service/index.ts
+++ src/service/index.ts
@@ -1,5 +1,3 @@
-//export { default as MainService } from "./MainService";
-
 export { default as UserService } from "./UserService";
 export { default as EvaluationService } from "./EvaluationService";
 export { default as ResultService } from "./ResultService";
