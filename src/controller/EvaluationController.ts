33b69384a914d1b15e1c17f3e6d938e4e3646103
dltjdn <hana40004@naver.com>
2022-11-20 01:25:46 +0900

    [ADD] 폴더 구조 세팅

---
+++ src/controller/EvaluationController.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+import { rm, sc } from "../constants";
+import { success, fail } from "../constants/response";
+
+const EvaluationController = {};
+
+export default EvaluationController;
--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,1 +1,4 @@
 // export { default as MainController } from "./MainController";
+export { default as UserController } from "./UserController";
+export { default as EvaluationController } from "./EvaluationController";
+export { default as ResultController } from "./ResultController";
