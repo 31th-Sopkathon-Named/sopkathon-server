222b208de16c0d459793cf09a371fcc0e0c32df5
dltjdn <hana40004@naver.com>
2022-11-20 03:52:00 +0900

    [feature/#2] 상대방과 매치 API

--- src/service/UserService.ts
+++ src/service/UserService.ts
@@ -1,6 +1,31 @@
+import { UserMatchDTO } from "./../interfaces/user/UserMatchDTO";
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-const UserService = {};
+//* 상대방과 매치
+const matchTwo = async (userMatchDTO: UserMatchDTO) => {
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        nickName: userMatchDTO.nickName,
+        phoneNum: userMatchDTO.phoneNum,
+      },
+    });
+
+    const data = {
+      fromId: userMatchDTO.myId,
+      toId: user.id,
+    };
+
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+const UserService = {
+  matchTwo,
+};
 
 export default UserService;
